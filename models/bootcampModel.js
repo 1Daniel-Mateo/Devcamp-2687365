bd3fd44f3de06a04e325808e7b7072d4f8346715
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2023-11-14 10:49:08 -1200

    crud con  validaciones

--- models/bootcampModel.js
+++ models/bootcampModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 //MODELOS CON LIBRERIAS
 //MODELO SOLO PARA MONGO
@@ -6,36 +6,32 @@ const mongoose = require('mongoose')
 // mongoose.shema es para crear esquemas de la crud
 
 const bootcampShema = new mongoose.Schema({
-    name:{
-        //se extiende el atributo
-        type: String,
-        unique: true,
-        required: [true, "el bootcamp esta repetido"],
-        maxlength: [20, "el nombre no puede ser superior a 20 caracteres"]
-    },
-    phone:{
-        type: Number,
-        required: [true, "el bootcamp esta repetido"],
-        maxlength: [10, "el telefono no puede ser superior a 10 caracteres"]
-    },
-    addres:{
-        type: String,
-        required: [true, "la direccion es requeridad"],
-    },
-    topics:{
-        type: [String],
-        enum: ["AI",
-    "Backend",
-"Frontend",
-"Devops"
-]
-    },
-    averageRating:Number,
-    createdAt:{
-        type:Date,
-        required: [true, "la feha es requerida"]
-    }
-})
+  name: {
+    //se extiende el atributo
+    type: String,
+    unique: true,
+    required: [true, "se rquiere nombre de bootcamp"],
+    maxlength: [20, "el nombre no puede ser superior a 20 caracteres"],
+  },
+  phone: {
+    type: Number,
+    required: [true, "se requiere numero de telefono"],
+    maxlength: [999999999, "el telefono no puede ser superior a 10 caracteres"],
+  },
+  address: {
+    type: String,
+    required: [true, "la direccion es requeridad"],
+  },
+  topics: {
+    type: [String],
+    enum: ["AI", "Backend", "Frontend", "Devops"],
+  },
+  averageRating: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Bootcamp = mongoose.model("Bootcamp",bootcampShema)
-module.exports = Bootcamp
+const Bootcamp = mongoose.model("Bootcamp", bootcampShema);
+module.exports = Bootcamp;
